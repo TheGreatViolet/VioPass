e9e225bb33fe02ac0b42bf4715e8d8352fa4ed39
Violet Hayes <ampc11234@gmail.com>
2021-12-29 15:03:16 -0800

    Style part of the Style and Security Update

--- main.py
+++ main.py
@@ -1,39 +1,78 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QBoxLayout, QGridLayout, QLabel, QLineEdit, QListWidget, QMessageBox, QPushButton, QMainWindow
-from PyQt5.QtCore import QLine
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 import passgen
+import webbrowser
+
+
+class About(QWidget):
+    def __init__(self, parent=None):
+        super(About, self).__init__(parent)
+        self.title='About VioPass'
+        self.setStyleSheet(open('stylesheet.css').read())
+        self.width=400
+        self.height=280
+        self.aboutUI()
+    
+    def aboutUI(self):
+        self.setWindowTitle(self.title)
+        self.setFixedSize(self.width, self.height)
+
+        self.label = QLabel("About VioPass:", self)
+        self.ver = QLabel("Version 2.0.0, Style and Security Update", self)
+        self.developer = QLabel("Developed by Violet Hayes", self)
+        self.license = QLabel("GNU GPL 3.0 License", self)
+
+        self.label.move(20, 20)
+        self.ver.move(20, 60)
+        self.developer.move(20, 80)
+        self.license.move(20, 100)
+
+        self.repobutton = QPushButton("View GitHub Repo", self)
+        self.repobutton.resize(140, 20)
+        self.repobutton.move(220, 240)
+
+        self.repobutton.clicked.connect(self.openrepo)
+
+    def openrepo(self):
+        webbrowser.open('https://github.com/TheGreatViolet/VioPass/tree/main')
 
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title='Openpass 0.1.0'
-        self.left=10
-        self.top=10
-        self.width=640
+        self.title='VioPass 2.0.0'
+        self.setStyleSheet(open('stylesheet.css').read())
+        self.width=660
         self.height=480
         self.masterPassPrompt()
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.setFixedSize(self.width, self.height)
 
         self.nameentry = QLineEdit(self)
-        self.nameentry.move(20,20)
+        self.nameentry.move(360,20)
         self.nameentry.resize(280,40)
         self.generatebutton = QPushButton('Generate Password', self)
-        self.generatebutton.move(20,80)
+        self.generatebutton.move(360,80)
         self.generatebutton.resize(140, 40)
 
         self.generatebutton.clicked.connect(self.ongen)
 
         self.passlist = QListWidget(self)
-        self.passlist.move(340,20)
-        self.passlist.resize(300,120)
+        self.passlist.move(0,0)
+        self.passlist.resize(240,480)
 
         self.passlist.itemClicked.connect(self.displayPassword)
 
         self.listPasswords()
+
+        self.aboutbutton = QPushButton('About', self)
+        self.aboutbutton.resize(60, 20)
+        self.aboutbutton.move(580, 440)
+
+        self.aboutbutton.clicked.connect(self.openabout)
 
         self.show()
 
@@ -58,7 +97,7 @@ class App(QMainWindow):
     def masterPassPrompt(self):
         passlist = passgen.loadPasswords()
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left,self.top,self.width,self.height)
+        self.setFixedSize(self.width, self.height)
         if "masterpass" in passlist.keys():
             self.prompt = QLabel("Please enter your master password", self)
             self.prompt.move(20,20)
@@ -67,7 +106,7 @@ class App(QMainWindow):
             self.promptbox.move(20,60)
             self.promptbox.resize(280,40)
             self.enterbutton = QPushButton("Enter", self)
-            self.enterbutton.move(20,100)
+            self.enterbutton.move(20,120)
 
             self.enterbutton.clicked.connect(self.checkMasterPass)
             
@@ -87,7 +126,7 @@ class App(QMainWindow):
             self.confirm.move(20,140)
             self.confirm.resize(600,40)
             self.enterbutton = QPushButton("Enter", self)
-            self.enterbutton.move(20, 180)
+            self.enterbutton.move(20, 200)
 
             self.enterbutton.clicked.connect(self.checkAndSave)
 
@@ -123,6 +162,17 @@ class App(QMainWindow):
             self.initUI()
         else:
             QMessageBox.warning(self, "Error", "The entered passwords don't match.")
+
+    def openabout(self):
+        self.aboutwindow = About()
+        self.aboutwindow.show()
+
+    def closeEvent(self, event):
+        widgetlist = QApplication.topLevelWidgets()
+        numwindows = len(widgetlist)
+        if numwindows > 1:
+            self.aboutwindow = About()
+            self.aboutwindow.close()
 
 
 def main():
